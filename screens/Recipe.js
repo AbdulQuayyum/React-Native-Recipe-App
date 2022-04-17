75ceff98730fac4d1c2bb956c5e2ad8a0ab700c6
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2022-04-17 23:56:33 +0100

    first commit

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    TouchableOpacity,
+    Animated,
+    Platform
 } from 'react-native';
+import { BlurView } from '@react-native-community/blur';
 
-const Recipe = () => {
+import { SIZES, FONTS, COLORS, icons } from "../constants"
+
+const HEADER_HEIGHT = 350;
+
+const Recipe = ({ navigation, route }) => {
+
+    const [selectedRecipe, setSelectedRecipe] = React.useState(null)
+
+    React.useEffect(() => {
+        let { recipe } = route.params
+        setSelectedRecipe(recipe)
+    }, [])
+
     return (
         <View
-            style={{
+            style={{ 
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                backgroundColor: COLORS.white
             }}
         >
-            <Text>Recipe</Text>
+            <Animated.FlatList
+            data={selectedRecipe?.ingredients}
+            keyExtractor={item =>`${item.id}`}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={
+                <View></View>
+            }
+            scrollEventThrottle={16}
+            onScroll={Animated.event([
+                { nativeEvent: { contentOffset: { y: scrollY } } }
+            ], { useNativeDriver: true } )}
+            />
         </View>
     )
 }
